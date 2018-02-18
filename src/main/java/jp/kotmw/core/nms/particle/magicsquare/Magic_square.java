9c07afbd837102ef0ad7696bf7d42381af9b9beb
motlof <supurasyu@hotmail.co.jp>
2018-02-19 02:03:00 +0900

    1.0.4 色パラメータの管理と指定方法をちょっと変更

--- src/main/java/jp/kotmw/core/nms/particle/magicsquare/Magic_square.java
+++ src/main/java/jp/kotmw/core/nms/particle/magicsquare/Magic_square.java
@@ -1,6 +1,7 @@
 package jp.kotmw.core.nms.particle.magicsquare;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -16,11 +17,21 @@ import jp.kotmw.core.nms.particle.magicsquare.Position.PositionType;
 
 public class Magic_square {
 	
+	/*
+	 * メモ
+	 * Graphics2かなんかで図形作ってから描画っての方が楽かもしれん
+	 * ただメモリ使用量は増えそうだが
+	 * 文字列の描画をしたい場合には楽・・・だと思うというかGraphics2Dしか出来なくない？
+	 * ちょっと面倒になる
+	 * 
+	 */
+	
 	private Location location;
 	private DetailsColor detailsColor;
 	
 	protected EnumParticle particle;
 	protected String color;
+	protected List<Double> params = new ArrayList<>();
 	protected List<ShapeData> shapes = new ArrayList<>();
 	
 	protected Magic_square() {}
@@ -31,6 +42,7 @@ public class Magic_square {
 		particle = square.particle;
 		color = square.color;
 		shapes = square.shapes;
+		params = square.params;
 	}
 
 	public void show() {
@@ -42,61 +54,65 @@ public class Magic_square {
 		Location location = data.getPosition() == null 
 				? this.location.clone() : this.location.clone().add(data.getPosition().getType() == PositionType.POLAR_COORDINATE 
 						? data.getPosition().getPolar_Coordinates(this.location.getWorld()).convertLocation() : data.getPosition().getLocation(this.location.getWorld()));
-		detailsColor = new DetailsColor(data.color == null ? color : data.color);
+		EnumParticle particle = (data.getParticle() == null ? this.particle : data.getParticle());
+		String colorcode = (data.color == null ? color : data.color);
+		if(params.size() < 4) params.addAll(Arrays.asList((double)(particle.hasColorParticle() ? 1 : 0), (double)0));
+		detailsColor = colorcode != null ? new DetailsColor(colorcode) : new DetailsColor(params.get(0).intValue(), params.get(1).intValue(), params.get(2).intValue());
 		switch (data.getType()) {
-		case CIRCLE: circle(data, location);
+		case CIRCLE: circle(data, location, particle);
 			break;
-		case POLYGON: polygon(data, location);
+		case POLYGON: polygon(data, location, particle);
 			break;
-		case STAR: star(data, location);
+		case STAR: star(data, location, particle);
 			break;
-		case LINE: line(data, location);
+		case LINE: line(data, location, particle);
 			break;
 		}
 	}
 	
 	//num 星型多角形
-	private void star(ShapeData data, Location location) {
+	private void star(ShapeData data, Location location, EnumParticle particle) {
 		for(double theta = 0.0; theta <= 2*Math.PI; theta += Math.PI/(data.getQuantity()/2)) {
 			Polar_coordinates pc = new Polar_coordinates(location.getWorld(), data.getRadius(), theta, 0);
 			double max = pc.convertLocation().distance(pc.clone().add(0, 2*Math.PI/(data.getQuantity()/2), 0).convertLocation());
 			for(double line = 0.0; line <= max; line += 0.2)
-				sendParticle(particle, location.clone().add(pc.convertLocation()).add(pc.clone().add(line-data.getRadius(), ((2+(data.getQuantity()/2))*Math.PI/((data.getQuantity()/2)*2)), 0).convertLocation()), detailsColor);
+				sendParticle(particle, location.clone().add(pc.convertLocation()).add(pc.clone().add(line-data.getRadius(), ((2+(data.getQuantity()/2))*Math.PI/((data.getQuantity()/2)*2)), 0).convertLocation()));
 		}
 	}
 	
 	//num 多角形
-	private void polygon(ShapeData data, Location location) {
+	private void polygon(ShapeData data, Location location, EnumParticle particle) {
 		for(double theta = 0.0; theta <= 2*Math.PI; theta += Math.PI/(data.getQuantity()/2)) {
 			Polar_coordinates pc = new Polar_coordinates(location.getWorld(), data.getRadius(), theta, 0);
 			double max = pc.convertLocation().distance(pc.clone().add(0, (Math.PI/(data.getQuantity()/2)), 0).convertLocation());
 			for(double line = 0.0; line <= max; line += 0.2)
-				sendParticle(particle, location.clone().add(pc.convertLocation()).add(pc.clone().add(line-data.getRadius(), Math.PI*((data.getQuantity()/2)+1)/(2*(data.getQuantity()/2)), 0).convertLocation()), detailsColor);
+				sendParticle(particle, location.clone().add(pc.convertLocation()).add(pc.clone().add(line-data.getRadius(), Math.PI*((data.getQuantity()/2)+1)/(2*(data.getQuantity()/2)), 0).convertLocation()));
 		}
 	}
 	
 	//丸
-	private void circle(ShapeData data, Location location) {
+	private void circle(ShapeData data, Location location, EnumParticle particle) {
 		for(double theta = 0.0; theta <= Math.PI*2; theta += Math.PI/60) {
-			sendParticle(particle, location.clone().add(new Polar_coordinates(location.getWorld(), data.getRadius(), theta, 0).convertLocation()), detailsColor);
+			sendParticle(particle, location.clone().add(new Polar_coordinates(location.getWorld(), data.getRadius(), theta, 0).convertLocation()));
 		}
 	}
 	
-	private void line(ShapeData data, Location location) {
+	//線
+	private void line(ShapeData data, Location location, EnumParticle particle) {
 		for(double radius = 0; radius <= data.getRadius(); radius += 0.1) {
-			sendParticle(particle, location.clone().add(new Polar_coordinates(location.getWorld(), radius, Math.toRadians(data.getPosition().getAngle()), 0).convertLocation()), detailsColor);
+			sendParticle((data.getParticle() == null ? particle : data.getParticle()), location.clone().add(new Polar_coordinates(location.getWorld(), radius, Math.toRadians(data.getPosition().getAngle()), 0).convertLocation()));
 		}
 	}
 	
-	private void sendParticle(EnumParticle particle, Location center, DetailsColor color) {
+	private void sendParticle(EnumParticle particle, Location center) {
 		if(particle.hasColorParticle()) {
-			sendParticle(particle, center, color.getRed(), color.getGreen(), color.getBlue());
+			sendParticle(particle, center, detailsColor.getRed(), detailsColor.getGreen(), detailsColor.getBlue(), 1, 0);
 			return;
 		}
-		sendParticle(particle, center, 0, 0, 0);
+		sendParticle(particle, center, params.get(0).floatValue(), params.get(1).floatValue(), params.get(2).floatValue(), params.get(3).floatValue(), (params.size() < 5 ? 0 : params.get(4).intValue()));
 	}
 	
-	private void sendParticle(EnumParticle particle, Location center, float x, float y, float z) {
+	private void sendParticle(EnumParticle particle, Location center, float x, float y, float z, float speed, int amount) {
 		Bukkit.getOnlinePlayers().stream().filter(player -> center.getWorld().getName().equals(player.getLocation().getWorld().getName()))
 		.forEach(player -> {
 			new ParticleAPI.Particle(particle, 
@@ -104,8 +120,8 @@ public class Magic_square {
 					x, 
 					y, 
 					z, 
-					1, 
-					0).sendParticle(player);
+					speed, 
+					amount).sendParticle(player);
 		});
 	}
 }
